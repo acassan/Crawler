43d50a558bd317a048b5a2f221662fae7990ef73
Alexis <alexis.cassan@riverline.fr>
2014-03-31 17:18:19 +0200

    iii

--- exploreCrawler.php
+++ exploreCrawler.php
@@ -3,7 +3,8 @@ require_once 'Crawler/ExploreCrawler.php';
 require_once 'Lib/Database.php';
 
 $db     = Database::getInstance();
-$sSql   = "SELECT * FROM directory";
+$now    = new \DateTime();
+$sSql   = sprintf("SELECT * FROM directory WHERE updatedAt <= '%s'", date('Y-m-d H:i:s',strtotime('-1 day')));
 
 foreach($db->query($sSql) as $directory) {
     $crawler = new ExploreCrawler(array(
@@ -16,6 +17,9 @@ foreach($db->query($sSql) as $directory) {
     $crawler->initDirectory($directory);
     $crawler->setURL($directory['url']);
     $crawler->go();
+
+    $sSql = sprintf("UPDATE directory SET updatedAt = '%s' WHERE id = %d", $now->format('Y-m-d H:i:s'), $directory['id']);
+    $db->query($sSql);
 
     unset($crawler);
 }
