96dda2ea5601b617e90c95e3737a8f6d422a20cb
Alexis <alexis.cassan@riverline.fr>
2014-04-02 12:22:20 +0200

    ii

--- Crawler/JacCrawler.php
+++ Crawler/JacCrawler.php
@@ -110,7 +110,7 @@ class JacCrawler extends BaseCrawler implements CrawlerInterface
 
         $description = $divNodes->item(2)->childNodes->item(2)->nodeValue;
 
-        preg_match("#sortie.php?j=([0-9]+)#Uis", $DocInfo->content, $tmpGameId);
+        preg_match('#sortie.php?j=([0-9]+)#Uis', $DocInfo->content, $tmpGameId);
         var_dump($tmpGameId);
     }
 
