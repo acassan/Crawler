713fb25551433097b86f19b56379666af5072a65
Alexis <alexis.cassan@riverline.fr>
2014-04-03 18:32:01 +0200

    iii

--- updateDictionary.php
+++ updateDictionary.php
@@ -2,19 +2,29 @@
 require_once 'Lib/Database.php';
 $db = Database::getInstance();
 
+$sSql           = "SELECT MAX(id) AS maxid FROM website";
+foreach($db->query($sSql) as $tmp) { $websiteIdMax = $tmp['maxid']; }
+echo "Max website ID : ". $websiteIdMax ." \n";
+echo "Truncate dictionary .. \n";
 $sSqlTruncate   = "TRUNCATE TABLE dictionary";
-$sSql           = "SELECT * FROM website_dictionary";
-
+$sSql           = "SELECT * FROM website_dictionary WHERE website_id = [id]";
 $dictionary     = array();
 
-foreach($db->query($sSql) as $websiteWord) {
-    die("ok");
-    if(!array_key_exists($websiteWord['word'], $dictionary)) {
-        $dictionary[$websiteWord['word']] = array();
+for($i=1; $i <= $websiteIdMax; $i++) {
+    echo "Handling website id ". $i ." .. \n";
+    foreach($db->query(str_replace("[id]", $i, $sSql)) as $websiteWord) {
+        if(!array_key_exists($websiteWord['word'], $dictionary)) {
+            $dictionary[$websiteWord['word']] = array();
+        }
+
+        $dictionary[$websiteWord['word']][] = intval($websiteWord['website_id']);
     }
 
-    $dictionary[$websiteWord['word']][] = intval($websiteWord['website_id']);
+    unset($websiteWord);
+    gc_collect_cycles();
 }
+
+
 
 foreach($dictionary as $word => $websites) {
     $dictionaryWord = array(
