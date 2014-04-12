83636b7690a05a2e0abde42dc6fa2dafc8ac4ef7
acassan <acassan@hotmail.fr>
2014-04-13 00:50:58 +0200

    azd

--- Lib/Tools.php
+++ Lib/Tools.php
@@ -12,6 +12,8 @@ Class Tools
         $replace    = array('a','a','a','a','a','c','e','e','e','e','i','i','i','i','n','o','o','o','o','o','u','u','u','u','y','y','A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','U','U','U','U','Y');
         $word       = str_replace($search,$replace,$word);
 
+        $word       = strtolower($word);
+
         $search     = array(',',';',':','/','?','.','!','*','$','^','&','"',"'",'(',')','{','}','[',']','|','`','#','=');
         $word       = str_replace($search,'',$word);
 
