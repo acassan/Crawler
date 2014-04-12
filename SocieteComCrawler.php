3d266c730321c631c771ffad3759373117384d8d
acassan <acassan@hotmail.fr>
2014-04-12 17:36:22 +0200

    azd

--- SocieteComCrawler.php
+++ SocieteComCrawler.php
@@ -10,7 +10,7 @@ $crawler = new SocieteComCrawler(array(
     'showPageRequested'         => false,
 ));
 
-//$crawler->setURL("http://www.societe.com");
-$crawler->setURL("http://www.societe.com/societe/riverline-535190920.html");
+$crawler->setURL("http://www.societe.com");
+//$crawler->setURL("http://www.societe.com/societe/riverline-535190920.html");
 $crawler->go();
 
