6d8b26fcef12b689bdc7e1970dbf4981c349a5ed
acassan <acassan@hotmail.fr>
2014-04-13 12:05:13 +0200

    azd

--- Crawler/SocieteComCrawler.php
+++ Crawler/SocieteComCrawler.php
@@ -83,6 +83,7 @@ class SocieteComCrawler extends BaseCrawler implements CrawlerInterface
                             break;
                             case "forme juridique":
                                 $societyData['legaltype'] = ltrim(rtrim($tr->getElementsByTagName('td')->item(1)->nodeValue));
+                                $societyData['legaltype'] = htmlentities($societyData['legaltype'],null,'UTF-8');
                             break;
                             case "siret":
                                 $societyData['siret'] = ltrim(rtrim($tr->getElementsByTagName('td')->item(1)->nodeValue));
